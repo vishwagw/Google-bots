b683b3bca208c6d077ecc4bc9f81498160f90fb0
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2026-01-19 00:14:16 +0530

    Update open_AI_bot.py

--- open_AI_bot.py
+++ open_AI_bot.py
@@ -1,3 +1,14 @@
 # sample chatbot with open AI API integration:
 import openai
 
+openai.api_key = 'my_api_key'
+
+message = {"role":"user", "content": input("This is the beginning of your chat with AI. [To exit, send \"###\".]\n\nYou:")};
+
+conversation = [{"role": "system", "content": "DIRECTIVE_FOR_gpt-3.5-turbo"}]
+while(message["content"]!="###"):
+    conversation.append(message)
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=conversation) 
+    message["content"] = input(f"Assistant: {completion.choices[0].message.content} \nYou:")
+    print()
+    conversation.append(completion.choices[0].message)
